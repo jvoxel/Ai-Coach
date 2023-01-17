897f0fb6bcaf6a732eda7e84171f79c1d49e8ced
Jvoxel <jvoxel@gmail.com>
2023-01-17 12:20:06 +0000

    My updates
    added main features of the app

---
+++ pages/api/generate.js
@@ -0,0 +1,56 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix =
+`
+Write a detailed gym routine list and focus 50% of the excercises on the first body part with workout bellow.
+
+workout:
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Take the list and workot of the Gym session below and generate a proffesionally designed workout session that last around an hour in style of Vince Gironda and Tom Platz. Make it feel like a story. Offer excercises that take in consideration the age and gender. depending on the difficulty stress the load weight realated to the 1 rep max and number of reps and sets. Go deep into each one.
+
+  Title: ${req.body.userInput}
+
+  Table of Contents: ${basePromptOutput.text}
+
+  Gym session:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,161 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import impactframesLogo from '../assets/impactframes-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  const [mod1, setMod1] = useState('');
+  const [mod2, setMod2] = useState('');
+  const [mod3, setMod3] = useState('');
+  const [mod4, setMod4] = useState('');
+  
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  const handleMod1Change = (e) => {
+    setMod1(e.target.value);
+  };
+
+  const handleMod2Change = (e) => {
+    setMod2(e.target.value);
+  };
+
+  const handleMod3Change = (e) => {
+    setMod3(e.target.value);
+  };
+
+  const handleMod4Change = (e) => {
+    setMod4(e.target.value);
+  };
+
+  const copyMod1ToInput = () => {
+    setUserInput(userInput + mod1);
+  };
+
+  const copyMod2ToInput = () => {
+    setUserInput(userInput + mod2);
+  };
+
+  const copyMod3ToInput = () => {
+    setUserInput(userInput + mod3);
+  };
+
+  const copyMod4ToInput = () => {
+    setUserInput(userInput + mod4);
+  };
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Ai Gym Personal Trainer</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Hi Let's plan your gym session</h2>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <br />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <select value={mod1} onChange={handleMod1Change}>
+              <option value="">Select split Focus</option>
+              <option value="Let's do a Legs, Arms and Abdomen split workout for ">Legs</option>
+              <option value="Let's do an Abdomen and Cardio split workout for ">Abdomen</option>
+              <option value="Let's do an Arms, Legs and Abdomen split workout for ">Arms</option>
+              <option value="Let's do a Chest, Back and Abdomen split workout for ">Chest</option>
+              <option value="Let's do a Back, Legs and Abdomen split workout for ">Back</option>
+              <option value="Let's do a Chest, Legs and Abdomen split workout for ">Chest</option>
+            </select>
+            <button onClick={copyMod1ToInput}>Copy to input</button>
+            <select value={mod2} onChange={handleMod2Change}>
+              <option value="">Gender</option>
+              <option value="a woman ">Female</option>
+              <option value="a man ">Male</option>
+            </select>
+            <button onClick={copyMod2ToInput}>Copy to input</button>
+            <select value={mod3} onChange={handleMod3Change}>
+              <option value="">age</option>
+              <option value="over 20 years old ">20+</option>
+              <option value="over 35 years old ">35+</option>
+              <option value="over 50 years old ">50+</option>
+            </select>
+            <button onClick={copyMod3ToInput}>Copy to input</button>
+            <select value={mod4} onChange={handleMod4Change}>
+              <option value="">Difficulty</option>
+              <option value="make it challenging ">Gym goer</option>
+              <option value="make it intense ">Shreded</option>
+              <option value="make it very intense ">Beast</option>
+            </select>
+            <button onClick={copyMod4ToInput}>Copy to input</button>
           </div>
-        </a>
+        <div className="prompt-container">
+          <textarea
+            placeholder="slect an split and enter the number of sets and reps"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {/* New code I added here */}
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="badge-container grow">
+          <a
+            href="https://impactframes.art"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="badge">
+              <Image src={impactframesLogo} alt="impactframes logo" />
+              <p>build by impactframes</p>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
